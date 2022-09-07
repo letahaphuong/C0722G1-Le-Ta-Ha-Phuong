b425bece34f54e5aace72f0ffbd167a62f715aaf
Your Name <you@example.com>
2022-09-07 18:58:51 +0700

    up

--- module2/src/Main.java
+++ module2/src/Main.java
@@ -3,11 +3,7 @@ import java.util.Scanner;
 public class Main {
 
 
-        static int m ( int...x){
-            int b = 0;
-            for (int a : x) {
-                b += a;
-            }
-            return b;
-        }
+    public static void main(String[] args) {
+
+    }
     }
