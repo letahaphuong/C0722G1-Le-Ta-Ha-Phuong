074d01f24652c128dbfd2dcc4edf0006da1cf4ed
Your Name <you@example.com>
2022-09-12 11:03:01 +0700

    upbai

--- module2/src/ss5_access_modifier_static_method_static_property/excersice/circle/TestCircle.java
+++ module2/src/ss5_access_modifier_static_method_static_property/excersice/circle/TestCircle.java
@@ -3,7 +3,6 @@ package ss5_access_modifier_static_method_static_property.excersice.circle;
 public class TestCircle {
     public static void main(String[] args) {
         Circle circle=new Circle();
-        circle.getRadius();
         circle.getArea();
         System.out.println(circle.getRadius()+circle.getArea());
     }
