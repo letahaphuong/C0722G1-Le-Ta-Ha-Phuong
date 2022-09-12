1ce515932b996b5ee54ac729cc6d20d08a26cdf1
Your Name <you@example.com>
2022-09-12 11:17:47 +0700

    upbai

--- module2/src/ss5_access_modifier_static_method_static_property/excersice/circle/Circle.java
+++ module2/src/ss5_access_modifier_static_method_static_property/excersice/circle/Circle.java
@@ -13,6 +13,10 @@ public class Circle {
         this.radius = radius;
     }
 
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
     public double getRadius() {
         return radius;
     }
--- module2/src/ss5_access_modifier_static_method_static_property/excersice/circle/TestCircle.java
+++ module2/src/ss5_access_modifier_static_method_static_property/excersice/circle/TestCircle.java
@@ -3,7 +3,7 @@ package ss5_access_modifier_static_method_static_property.excersice.circle;
 public class TestCircle {
     public static void main(String[] args) {
         Circle circle = new Circle();
-        circle.getArea();
-        System.out.println(circle.getRadius() + circle.getArea());
+        circle.setRadius(5);
+        System.out.println("Diện tích hình tròn là: "+circle.getArea()+"\nBán kính hình tròn là: "+circle.getRadius());
     }
 }
