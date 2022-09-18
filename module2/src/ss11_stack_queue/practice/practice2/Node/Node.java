944d5263873fd4d58752f2d9e8003104df51708d
Your Name <you@example.com>
2022-09-18 17:22:13 +0700

    up

---
+++ module2/src/ss11_stack_queue/practice/practice2/Node/Node.java
@@ -0,0 +1,12 @@
+package ss11_stack_queue.practice.practice2;
+
+public class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+    public void 
+}
