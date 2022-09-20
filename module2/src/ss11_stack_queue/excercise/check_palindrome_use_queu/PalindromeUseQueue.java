374a65c2246292d2b0ab7cac8aa6333246d3ea8b
Your Name <you@example.com>
2022-09-20 10:26:12 +0700

    upbai

--- module2/src/ss11_stack_queue/excercise/check_palindrome_use_queu/PalindromeUseQueue.java
+++ module2/src/ss11_stack_queue/excercise/check_palindrome_use_queu/PalindromeUseQueue.java
@@ -1,28 +1,29 @@
 package ss11_stack_queue.excercise.check_palindrome_use_queu;
 
 import java.util.ArrayDeque;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
 public class PalindromeUseQueue {
     public static void main(String[] args) {
-        String str = "Able was I ere I saw Elba";
+        String str = "123321";
         str = str.toUpperCase();
         String[] array;
         array = str.split("");
         Stack<String> stack = new Stack<>();
-        Queue<String> queue = new ArrayDeque<>();
+        Queue<String> queue = new LinkedList<>();
         for (int i = 0; i < array.length; i++) {
             stack.push(array[i]);
             queue.offer(array[i]);
         }
-        boolean isCheck = true;
+        boolean isCheck = false;
         for (int i = 0; i < stack.size(); i++) {
-            if (queue.poll().equals(stack.pop())) {
-                isCheck = true;
+            if (!queue.poll().equals(stack.pop())) {
+                isCheck = false;
                 break;
             }
-            isCheck = false;
+            isCheck = true;
         }
         if (isCheck){
             System.out.println("Ok");
--- module2/src/ss11_stack_queue/excercise/element_count/MapTree.java
+++ module2/src/ss11_stack_queue/excercise/element_count/MapTree.java
@@ -4,24 +4,24 @@ import java.util.*;
 
 public class MapTree {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập thông tin: ");
-        String str=scanner.nextLine();
+        String str = scanner.nextLine();
         str = str.toUpperCase();
         System.out.println(str);
-        String[] str1=str.split("");
+        String[] str1 = str.split("");
         System.out.println(Arrays.toString(str1));
-        Stack<String> temp=new Stack<>();
+        Stack<String> temp = new Stack<>();
         for (int i = 0; i < str1.length; i++) {
             temp.push(str1[i]);
         }
-        Map<String, Integer> map =new TreeMap<>();
+        Map<String, Integer> map = new TreeMap<>();
         for (int i = 0; i < temp.size(); i++) {
-           if (!map.containsKey(temp.get(i))){
-               map.put(temp.get(i),1);
-           }else {
-               map.put(temp.get(i),map.get(temp.get(i))+1);
-           }
+            if (!map.containsKey(temp.get(i))) {
+                map.put(temp.get(i), 1);
+            } else {
+                map.put(temp.get(i), map.get(temp.get(i)) + 1);
+            }
         }
         System.out.println(map);
     }
