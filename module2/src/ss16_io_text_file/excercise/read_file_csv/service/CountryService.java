85ee6422e5b0c8b9b1459a765d1da442314a1b0c
Your Name <you@example.com>
2022-09-29 19:56:00 +0700

    up

--- module2/src/ss16_io_text_file/excercise/read_file_csv/service/CountryService.java
+++ module2/src/ss16_io_text_file/excercise/read_file_csv/service/CountryService.java
@@ -7,32 +7,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CountryService {
-    public static List<Country> readFile(String filePath) throws IOException {
+    public static List<Country> readFile(String filePath) {
         List<Country> countryList = new ArrayList<>();
-        File file = new File(filePath);
-        FileReader fileReader = new FileReader(file);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String line;
-        String[] info;
-        Country country;
-        while ((line = bufferedReader.readLine()) != null) {
-            info = line.split(",");
-//            country=new Country(Integer.parseInt(info[0]),info[1],info[2]);
-            country = new Country();
-            country.setId(Integer.parseInt(info[0]));
-            country.setCode(info[1]);
-            country.setName(info[2]);
-            countryList.add(country);
+        try {
+            File file = new File("src\\ss16_io_text_file\\excercise\\read_file_csv\\data\\test.txt");
+            FileReader fileReader = new FileReader(file);
+            if (!file.exists()) {
+                System.out.println("file không tồn tại!");
+            }
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            String[] info;
+            Country country;
+            while ((line = bufferedReader.readLine()) != null) {
+                info = line.split(",");
+                country = new Country();
+                country.setId(Integer.parseInt(info[0]));
+                country.setCode(info[1]);
+                country.setName(info[2]);
+                countryList.add(country);
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.getStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        bufferedReader.close();
+
         return countryList;
     }
 
     public static void infoCountry(List<Country> countryList) {
-        System.out.println("Tên các Quốc Gia!");
-
         for (Country country : countryList) {
-            System.out.println(country.getId() +" : "+ country.getName());
+            System.out.println(country.getId() + " : " + country.getName());
         }
     }
 }
