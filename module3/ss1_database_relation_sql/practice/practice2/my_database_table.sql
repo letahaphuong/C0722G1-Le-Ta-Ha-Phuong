7c90e4869c063cea802ed1a3af92e7e8395e4363
Your Name <you@example.com>
2022-10-14 11:18:03 +0700

    up

--- module3/ss1_database_relation_sql/practice/practice2/my_database_table.sql
+++ module3/ss1_database_relation_sql/practice/practice2/my_database_table.sql
@@ -1,13 +1,40 @@
-create database if not exists my_database;
-create table student(
+create database if not exists student_management;
+use student_management;
+create table class(
 id int,
-`name` varchar(40),
-age int,
-email varchar(40)
+`name` varchar(50)
 );
+alter table class
+add primary key (id);
+insert into class (
+	id,
+    `name`
+) value (1,'C0722G1');
+select `name` from class;
+
+-- Teacher
 create table teacher(
-id int,
-`name` varchar(49),
+id int primary key,
+`name` varchar(50),
 age int,
-email varchar(40)
-)
+country varchar(50)
+);
+alter table teacher 
+add primary key (id);
+insert into teacher(
+id,
+`name`,
+age,
+country
+)value(1,"Nguyen phuoc sang",18,'vietnamese'),
+	  (2,"Le ta ha phuong",19,'canada');
+update teacher
+set country = 'vietnamese'
+where id=2;
+
+set SQL_SAFE_UPDATES =0;
+update teacher
+set country="Japan";
+set SQL_SAFE_UPDATES =1;
+update teacher
+set `name`="Nguyen tan " where id=2;
