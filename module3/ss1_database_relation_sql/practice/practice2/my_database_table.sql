f30f690a70a1babc5e1934df612abb63b8a5775b
Your Name <you@example.com>
2022-10-14 11:16:04 +0700

    update

---
+++ module3/ss1_database_relation_sql/practice/practice2/my_database_table.sql
@@ -0,0 +1,13 @@
+create database if not exists my_database;
+create table student(
+id int,
+`name` varchar(40),
+age int,
+email varchar(40)
+);
+create table teacher(
+id int,
+`name` varchar(49),
+age int,
+email varchar(40)
+)
