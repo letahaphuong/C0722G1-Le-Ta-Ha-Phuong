410f3fd1ce32b66bde2df693ba9e0401b40e4056
Your Name <you@example.com>
2022-10-17 11:12:52 +0700

    up_excersice

--- module3/ss2_design_and_create_database/excersice/exc2/code_for_diagram.sql
+++ module3/ss2_design_and_create_database/excersice/exc2/code_for_diagram.sql
@@ -1,30 +1,30 @@
 CREATE DATABASE IF NOT EXISTS management_sell;
 USE management_sell;
 CREATE TABLE customer(
-customer_id INT PRIMARY KEY,
+customer_id INT AUTO_INCREMENT PRIMARY KEY,
 customer_name VARCHAR(50),
 customer_age INT
 );
+
+CREATE TABLE `order`(
+order_id INT PRIMARY KEY,
+customer_id INT,
+FOREIGN KEY (customer_id)REFERENCES customer(customer_id),
+order_date DATE,
+order_total_price DOUBLE
+);
+
 CREATE TABLE product(
-product_id INT PRIMARY KEY,
+product_id INT AUTO_INCREMENT PRIMARY KEY,
 product_name VARCHAR(50),
 product_price DOUBLE
 );
-CREATE TABLE order_detail (
-    order_id INT PRIMARY KEY,
-    product_id INT,
-    FOREIGN KEY (product_id)
-        REFERENCES product (product_id),
-    order_QTY INT
-);
-CREATE TABLE `order` (
-    order_id INT PRIMARY KEY,
-    FOREIGN KEY (order_id)
-        REFERENCES order_detail (order_id),
-    customer_id INT,
-    FOREIGN KEY (customer_id)
-        REFERENCES customer (customer_id),
-    order_date DATE,
-    order_total_price DOUBLE
-);
 
+CREATE TABLE order_detail(
+order_id INT,
+product_id INT,
+PRIMARY KEY(order_id,product_id),
+FOREIGN KEY(order_id)REFERENCES `order`(order_id),
+FOREIGN KEY(product_id)REFERENCES product(product_id),
+order_QTY INT 
+);
